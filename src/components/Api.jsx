46295bd71ab40c65e49ec77da7e34671592aada5
FaijanShekh06 <zaduu0301@gmail.com>
2025-08-29 21:37:08 +0530

    React js Basics

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Api = () => {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    setData(response.data);
+    console.log(response.data);
+  };
   return (
-    <div className="flex flex-col items-center justify-center my-10 active:scale-95">
-      <button className="bg-cyan-900 text-amber-50 font-semibold text-2xl px-6 py-3 rounded-lg mt-10 hover:bg-cyan-700">
+    <div className="inline-block w-full items-center justify-center text-center">
+      <button
+        onClick={getData}
+        className="bg-cyan-900 text-amber-50 font-semibold text-2xl px-6 py-3 rounded-lg mt-10 hover:bg-cyan-700"
+      >
         Get Data
       </button>
-      <div className="p-5 bg-gray-900">Hello</div>
+      <div className="p-10 m-10 bg-gray-900">
+        {data.map((elem, id) => {
+          return (
+            <div className="inline-block m-4 p-4 bg-cyan-100 text-black rounded-lg shadow-lg  hover:scale-105 hover:shadow-2xl duration-300">
+              <img
+                key={id}
+                className="m-2 h-48 w-48 rounded-lg"
+                src={elem.download_url}
+                alt="img"
+              />
+              <h1 className="text-lg font-bold">{elem.author}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const Card = (props) => {
-  console.log(props.photue);
-
   return (
     <div className="bg-white text-black inline-block p-2 text-center rounded font-bold m-4 shadow-lg hover:scale-105 hover:shadow-2xl hover:bg-green-100 duration-300 w-[255px]">
       <img
